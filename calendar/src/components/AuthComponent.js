7b25b9d66d8fa818bdcc2a489f6e2a0cbd011e5c
Natalya Stepanova <88579535+StepanovaNatalya@users.noreply.github.com>
2022-07-14 11:34:06 +0200

    add authcomponent style

--- calendar/src/components/AuthComponent.js
+++ calendar/src/components/AuthComponent.js
@@ -1,6 +1,24 @@
-const AuthComponent = () =>{
-    return(
-        <h2>Auth</h2>
+import './../css/AuthComponent.css'
+
+const AuthComponent = () => {
+    return (
+        <div className='main-auth'>
+            <div className='container-auth'>
+                <div className='top-auth'>
+                    <p>Sign In</p>
+                </div>
+                <form>
+                    <input placeholder='Username' />
+                    <input placeholder='Password' />
+                    <p>Forgot <span>Username / Password?</span></p>
+                    <button className='btn-login-auth'>sign in</button>
+                    <div className='bottom-auth'>
+                        <p>Don't have an account?</p>
+                        <button>sign up now</button>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
